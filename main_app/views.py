22d130a9a601380e8f36cc9c54a96c9709c92032
Cirrus Mokhtari <Cirrus1994@gmail.com>
2020-10-30 16:28:36 -0700

    created profile index route and view

--- main_app/views.py
+++ main_app/views.py
@@ -7,6 +7,9 @@ from django.contrib.auth.forms import UserCreationForm
 def home(request):
     return render(request, 'home.html')
 
+def profiles_index(request):
+    return render(request, 'profiles/index.html')
+
 def signup(request):
     form = UserCreationForm()
     return render(request, 'registration/signup.html', {'form' : form})
