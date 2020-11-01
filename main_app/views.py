5c78c3326744590420b646c4f10c99bd18ad354b
Cirrus Mokhtari <Cirrus1994@gmail.com>
2020-11-01 10:55:11 -0800

    signup login logout work but redirect to wrong page

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from . models import Profile
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 # Create your views here.
 
@@ -10,12 +11,31 @@ def home(request):
 def profiles_index(request):
     return render(request, 'profiles/index.html')
 
-def signup(request):
-    form = UserCreationForm()
-    return render(request, 'registration/signup.html', {'form' : form})
-
 def profiles_detail(request, profile_id):
     profile = Profile.objects.get(id=profile_id)
     context = {'profile' : profile}
 
     return render(request, 'profiles/detail.html', context )
+
+def signup(request):
+    error_message = ''
+
+## Handling POST request to signup
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('profiles_index')
+        else:
+            error_message = 'Invalid sign up - please try again'
+            form = UserCreationForm()
+            context = {'form' : form, 'error_message' : error_message}
+            return render(request, 'registration/signup.html', context)
+    
+## Handling GET request to signup
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
+    
+    return render(request, 'registration/signup.html', {'form' : form})
