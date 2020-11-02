0722f4502c4ef5d4499fab5aaecff4387a0b230e
Cirrus Mokhtari <Cirrus1994@gmail.com>
2020-11-02 10:46:25 -0800

    fixed image and navbar issues

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,8 @@
 from . import views
 from django.urls import path, include
 from django.contrib import admin
+from django.conf.urls.static import static 
+from django.conf import settings
 
 urlpatterns = [
     path('', views.home, name="home"),
@@ -14,4 +16,4 @@ urlpatterns = [
 
     path('accounts/signup', views.signup, name='signup')
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
