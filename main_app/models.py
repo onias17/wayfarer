012ad48d30af08087cbcdf5119b8413cf53dcfc5
Cirrus Mokhtari <Cirrus1994@gmail.com>
2020-11-02 16:13:27 -0800

    New Cities and city detail pages/routes/views made

--- main_app/models.py
+++ main_app/models.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 
 # Create your models here.
 
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     picture = models.ImageField(upload_to="images/", default='/images/default-profile-img.png')
@@ -14,15 +15,24 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.username
 
+
+class City(models.Model):
+    name = models.CharField(max_length=50)
+    picture = models.ImageField(upload_to="images/", blank = True)
+    country = models.CharField(max_length=50)
+
+def __str__(self):
+    return self.name
+
+
 class Post(models.Model):
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
     content = models.TextField(max_length=1000)
     title = models.CharField(max_length= 100)
-    city = models.CharField(max_length=100) ##This will be changed to foreign key when cities model is made
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now_add=True)
 
 
-# class City(models.Model):
-#     name = models.CharField(max_length=50)
-#     picture = models.ImageField(upload)
+
+
 
--- main_app/views.py
+++ main_app/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
-from .forms import ProfileCreationForm, PostCreationForm
-from .models import Profile, Post
+from .forms import ProfileCreationForm, PostCreationForm, CityCreationForm
+from .models import Profile, Post, City
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
@@ -93,3 +93,22 @@ def posts_detail(request, post_id):
     post = Post.objects.get(id=post_id)
     context = {'post': post}
     return render(request, 'posts/detail.html', context)
+
+
+def add_city(request):
+    if request.method == "POST":
+        form = CityCreationForm(request.POST)
+
+        if form.is_valid():
+            new_city = form.save()
+            new_city.save()
+            return redirect('citydetail', new_city.id)
+        
+    else :
+        form = CityCreationForm()
+        return render(request, 'cities/new.html', {'form': form})
+
+def city_detail(request, city_id):
+    city = City.objects.get(id=city_id)
+    context = {'city': city}
+    return render(request, 'cities/detail.html', context)
