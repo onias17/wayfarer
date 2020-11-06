aa7828c64fa9b45f6bb546ef11f661f577fcc397
Cirrus Mokhtari <Cirrus1994@gmail.com>
2020-11-06 02:19:05 -0800

    before merging with daniel

--- main_app/models.py
+++ main_app/models.py
@@ -23,7 +23,7 @@ class Profile(models.Model):
 
 
 class City(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, blank=False)
     picture = models.ImageField(upload_to="images/", blank = True, default='/images/shea-rouda-Vtl6cOhO87Y-unsplash.jpg')
     country = models.CharField(max_length=50)
     slug = models.SlugField(null=True, default="slug")
